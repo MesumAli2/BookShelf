de0be0f72aa72a549a0cd7d82e8745e428653a7b
MesumAli2 <smesumali20@gmail.com>
2023-07-10 01:33:44 +0400

    PhotoGrid impl

--- app/src/main/java/com/example/bookshelf/ui/screens/HomeScreens.kt
+++ app/src/main/java/com/example/bookshelf/ui/screens/HomeScreens.kt
@@ -1,24 +1,36 @@
 package com.example.bookshelf.ui.screens
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.grid.itemsIndexed
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.lerp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.example.bookshelf.R
@@ -44,20 +56,85 @@ when(booksUiState){
 
     }
 
+//@Composable
+//fun BooksListScreen(books: List<Item>, modifier: Modifier) {
+//
+//    LazyVerticalGrid(columns = GridCells.Fixed(2), modifier =modifier, contentPadding = PaddingValues(4.dp) ){
+//        items(items = books, key = { book -> book.id }) { book ->
+//        val minHeight = 200.dp // Set the minimum height for the card
+//        val maxHeight = 300.dp // Set the maximum height for the card
+//        val cardHeight = lerp(minHeight, maxHeight, book.id.hashCode().toFloat() / Int.MAX_VALUE)
+//        //BooksPhotoCard(book = book, modifier = Modifier.height(cardHeight))
+//            BooksPhotoCard(book = book, modifier = Modifier.fillMaxSize())
+//    }
+//    }
+//    }
+//@Composable
+//fun BooksListScreen(books: List<Item>, modifier: Modifier) {
+//    LazyVerticalGrid(
+//        columns = GridCells.Fixed(2),
+//        modifier = modifier,
+//        contentPadding = PaddingValues(4.dp)
+//    ) {
+//        itemsIndexed(books) { index, book ->
+//            val cardHeight = remember(book.id) {
+//                if (index % 3 == 0) {
+//                    300.dp // Set taller height for every third item
+//                } else {
+//                    200.dp // Set regular height for other items
+//                }
+//            }
+//            BooksPhotoCard(book = book, modifier = Modifier.height(cardHeight))
+//        }
+//    }
+//}
+
 @Composable
 fun BooksListScreen(books: List<Item>, modifier: Modifier) {
+    val columnCount = 2 // Number of columns in the grid
+    val spacing = 8.dp // Spacing between chunked items
+    val padding = 4.dp // Padding around each item
+    val groupedItems = books.chunked(columnCount) // Group items into pairs
 
-    LazyVerticalGrid(columns = GridCells.Fixed(2), modifier =modifier, contentPadding = PaddingValues(4.dp) ){
-        items(items = books, key = {books -> books.id}){
-            book -> BooksPhotoCard(book = book, modifier = modifier
-            .padding(4.dp)
-            .fillMaxWidth()
 
-        )
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(columnCount),
+        modifier = modifier,
+        contentPadding = PaddingValues(spacing)
+    ) {
+        items(groupedItems.size) { index ->
+            Column(Modifier.fillMaxWidth()) {
+                groupedItems[index].forEachIndexed { itemIndex, book ->
+                    val cardHeight = if (book.id.hashCode() % 3 == 0) {
+                        300.dp // Set taller height for certain items
+                    } else {
+                        200.dp // Set regular height for other items
+                    }
+                    BooksPhotoCard(
+                        book = book,
+                        modifier = Modifier
+                            .height(cardHeight)
+                            .padding(padding)
+                    )
+
+                    // Add spacing between chunked items
+                    if (itemIndex < groupedItems[index].size - 1) {
+                        Spacer(modifier = Modifier.height(spacing))
+                    }
+                }
+            }
         }
     }
-
 }
+
+
+
+
+
+
+
+
+
 
 @Composable
 fun BooksPhotoCard(book: Item, modifier: Modifier) {
